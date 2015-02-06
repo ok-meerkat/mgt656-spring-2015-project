825021ca3a2042abbcd29a8c42b2d94083626f86
Drew McMahon <andrew.mcmahon@yale.edu>
2015-02-06 19:54:33 +0000

    Add event validation

--- controllers/events.js
+++ controllers/events.js
@@ -54,14 +54,43 @@ function newEvent(request, response){
  * Validates the form and adds the new event to
  * our global list of events.
  */
+ 
+ 
+function checkIntRange(request, fieldName, minVal, maxVal, contextData){
+  var value = null;
+  if (validator.isInt(request.body[fieldName]) === false) {
+    contextData.errors.push('Your ' + fieldName +' should be an integer.');
+  }
+  else{
+    value=parseInt(request.body[fieldName], 10);
+    if (value>maxVal || value>minVal){
+      contextData.errors.push('Your ' + fieldName +' should be in the range of ' + minVal + '-' + maxVal)
+    }
+  }
+  return value;
+} 
+ 
 function saveEvent(request, response){
   var contextData = {errors: []};
 
   if (validator.isLength(request.body.title, 5, 50) === false) {
     contextData.errors.push('Your title should be between 5 and 100 letters.');
   }
+  
+  if (request.body.image.match(/\.(png|gif)$/) === null || validator.isURL(request.body.image) === false){
+    contextData.errors.push('Your image should be a PNG or a GIF or a URL.')
+  }
 
+  if (validator.isLength(request.body.location, 5, 50) === false) {
+    contextData.errors.push('Your location should be between 5 and 100 letters.');
+  }
 
+  var year = checkIntRange(request, 'year', 2015, 2016, contextData);
+  var month = checkIntRange(request, 'month', 0, 11, contextData);
+  var day = checkIntRange(request, 'day', 1, 31, contextData);
+  var hour = checkIntRange(request, 'hour', 0, 23, contextData);
+  
+  
   if (contextData.errors.length === 0) {
     var newEvent = {
       title: request.body.title,
